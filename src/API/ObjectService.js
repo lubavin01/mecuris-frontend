835120c0b0e4f1cc9695eb2d8a379667016f3b7b
Lyubavin Nikolay <lubavin.n@n-t.io>
2022-05-18 11:52:01 +0300

    fix - URLs fixed

--- src/API/ObjectService.js
+++ src/API/ObjectService.js
@@ -1,25 +1,27 @@
 import axios from 'axios';
 
+const BASE_URL = 'http://localhost:5000/api/v1';
+
 export class ObjectService {
     static async getEntries() {
-        const { data } = await axios.get('http://localhost:5000/entries');
+        const { data } = await axios.get('/entries', { baseURL: BASE_URL });
 
         return data.data;
     }
 
     static async createEntry(entry) {
         const { color = '', width = 0, height = 0, depth = 0, positionX = 0, positionY = 0, positionZ = 0 } = entry;
-        const { data } = await axios.post('http://localhost:5000/entries', { color, width, height, depth, positionX, positionY, positionZ });
+        const { data } = await axios.post('/entries', { color, width, height, depth, positionX, positionY, positionZ }, { baseURL: BASE_URL });
         return data.data;
     }
 
     static async updateEntry(entry) {
-        const { _id: id, color = '', width = 0, height = 0, depth = 0, positionX = 0, positionY = 0, positionZ = 0 } = entry;
-        await axios.put('http://localhost:5000/entries', { id, color, width, height, depth, positionX, positionY, positionZ });
+        const { _id: entryId, color = '', width = 0, height = 0, depth = 0, positionX = 0, positionY = 0, positionZ = 0 } = entry;
+        await axios.put(`/entries/${entryId}`, { color, width, height, depth, positionX, positionY, positionZ }, { baseURL: BASE_URL });
 
     }
 
     static async deleteEntry(entryId) {
-        await axios.delete(`http://localhost:5000/entries/${entryId}`);
+        await axios.delete(`/entries/${entryId}`, { baseURL: BASE_URL });
     }
 }
